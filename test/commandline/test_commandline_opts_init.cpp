7d5aa0d2715e9ed66964d698ba76f725504072f3
Justin Handville <nanolith@gmail.com>
2022-03-07 02:02:44 -0500

    Added unit tests for -R argument.

--- test/commandline/test_commandline_opts_init.cpp
+++ test/commandline/test_commandline_opts_init.cpp
@@ -711,3 +711,180 @@ TEST(o_argument_multiple)
     dispose((disposable_t*)&f);
     dispose((disposable_t*)&alloc_opts);
 }
+
+/* If a -R is passed as an argument, the number of rounds can be changed. */
+TEST(R_argument)
+{
+    allocator_options_t alloc_opts;
+    vccrypt_suite_options_t suite;
+    file f;
+    vccert_builder_options_t builder_opts;
+    string exe_name = "vctool";
+    string rounds_argument = "-R";
+    string rounds_number = "4999";
+    string help_argument = "help";
+    char* argv[] = {
+        (char*)exe_name.c_str(), (char*)rounds_argument.c_str(),
+        (char*)rounds_number.c_str(), (char*)help_argument.c_str() };
+    int argc = sizeof(argv) / sizeof(char*);
+    commandline_opts opts;
+
+    /* register the mock crypto suite. */
+    vccrypt_suite_register_mock();
+
+    /* create malloc allocator. */
+    malloc_allocator_options_init(&alloc_opts);
+
+    /* create the mock file. */
+    TEST_ASSERT(
+        VCTOOL_STATUS_SUCCESS ==
+            file_mock_init(
+                &f,
+                /* stat. */
+                [&](file*, const char*, file_stat_st*) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* open. */
+                [&](file*, int*, const char*, int, mode_t) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* close. */
+                [&](file*, int) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* read. */
+                [&](file*, int, void*, size_t, size_t*) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* write. */
+                [&](
+                    file*, int, const void*, size_t, size_t*) -> int {
+                        return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* lseek. */
+                [&](file*, int, off_t, file_lseek_whence, off_t*) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                [&](file*, int) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                }));
+
+    /* create a mock crypto suite. */
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS ==
+        vccrypt_mock_suite_options_init(
+            &suite, &alloc_opts));
+
+    /* create a builder options instance. */
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS ==
+            vccert_builder_options_init(
+                &builder_opts, &alloc_opts, &suite));
+
+    /* calling commandline_opts_init should succeed. */
+    TEST_ASSERT(
+        VCTOOL_STATUS_SUCCESS ==
+            commandline_opts_init(
+                &opts, &f, &suite, &builder_opts, argc, argv));
+
+    /* the help command is set. */
+    TEST_ASSERT(NULL != opts.cmd);
+
+    /* get the root command. */
+    command* cmd = opts.cmd;
+    while (cmd->next != NULL) cmd = cmd->next;
+    root_command* root = (root_command*)cmd;
+
+    /* the root command rounds number is set. */
+    TEST_EXPECT(4999 == root->key_derivation_rounds);
+
+    /* clean up. */
+    dispose((disposable_t*)&opts);
+    dispose((disposable_t*)&builder_opts);
+    dispose((disposable_t*)&suite);
+    dispose((disposable_t*)&f);
+    dispose((disposable_t*)&alloc_opts);
+}
+
+/* If a -R is passed as an argument, the number must be greater than zero. */
+TEST(R_range_check)
+{
+    allocator_options_t alloc_opts;
+    vccrypt_suite_options_t suite;
+    file f;
+    vccert_builder_options_t builder_opts;
+    string exe_name = "vctool";
+    string rounds_argument = "-R";
+    string rounds_number = "-4999";
+    string help_argument = "help";
+    char* argv[] = {
+        (char*)exe_name.c_str(), (char*)rounds_argument.c_str(),
+        (char*)rounds_number.c_str(), (char*)help_argument.c_str() };
+    int argc = sizeof(argv) / sizeof(char*);
+    commandline_opts opts;
+
+    /* register the mock crypto suite. */
+    vccrypt_suite_register_mock();
+
+    /* create malloc allocator. */
+    malloc_allocator_options_init(&alloc_opts);
+
+    /* create the mock file. */
+    TEST_ASSERT(
+        VCTOOL_STATUS_SUCCESS ==
+            file_mock_init(
+                &f,
+                /* stat. */
+                [&](file*, const char*, file_stat_st*) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* open. */
+                [&](file*, int*, const char*, int, mode_t) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* close. */
+                [&](file*, int) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* read. */
+                [&](file*, int, void*, size_t, size_t*) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* write. */
+                [&](
+                    file*, int, const void*, size_t, size_t*) -> int {
+                        return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                /* lseek. */
+                [&](file*, int, off_t, file_lseek_whence, off_t*) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                },
+                [&](file*, int) -> int {
+                    return VCTOOL_ERROR_FILE_BAD_DESCRIPTOR;
+                }));
+
+    /* create a mock crypto suite. */
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS ==
+        vccrypt_mock_suite_options_init(
+            &suite, &alloc_opts));
+
+    /* create a builder options instance. */
+    TEST_ASSERT(
+        VCCRYPT_STATUS_SUCCESS ==
+            vccert_builder_options_init(
+                &builder_opts, &alloc_opts, &suite));
+
+    /* calling commandline_opts_init should succeed. */
+    TEST_ASSERT(
+        VCTOOL_STATUS_SUCCESS !=
+            commandline_opts_init(
+                &opts, &f, &suite, &builder_opts, argc, argv));
+
+    /* clean up. */
+    dispose((disposable_t*)&opts);
+    dispose((disposable_t*)&builder_opts);
+    dispose((disposable_t*)&suite);
+    dispose((disposable_t*)&f);
+    dispose((disposable_t*)&alloc_opts);
+}
